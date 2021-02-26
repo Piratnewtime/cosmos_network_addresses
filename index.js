31777749d3778a0771f83c84b5cda2f050acd0b7
Pavel <himyl@ya.ru>
2021-02-26 20:41:09 +0400

    fix spaces

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports.getAddressFromTx = function getAddressFromTx (tx, prefix = 'cosmo
     tx.signatures.forEach(function({pub_key: {value}}){
         const publicKey = Buffer.from(value, 'base64'); // https://github.com/cosmostation/cosmosjs/blob/157b350127e88f0b42fd1f500997c81829a9085a/src/index.js#L50
         const identifier = hash160(publicKey); // https://github.com/bitcoinjs/bip32/blob/master/src/bip32.js#L65
-	    list.push(encode(prefix, toWords(identifier))); // https://github.com/cosmostation/cosmosjs/blob/157b350127e88f0b42fd1f500997c81829a9085a/src/index.js#L109
+        list.push(encode(prefix, toWords(identifier))); // https://github.com/cosmostation/cosmosjs/blob/157b350127e88f0b42fd1f500997c81829a9085a/src/index.js#L109
     });
     return list;
 }
